a40f2d17bbe1263ed187709e953e5942a20f5acc
Rob Myers <rob@robmyers.org>
2015-12-21 12:22:55 -0800

    Fix disclosude in docs.

--- doc/script.js
+++ doc/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $('h5').css('cursor','pointer');
-  $('h5').nextUntil('h2').slideToggle();
-  $('h5').click(function() {$(this).nextUntil('h5').slideToggle();} );
+  $('h5').nextUntil('h5,h4,h3,h2,h1').slideToggle();
+  $('h5').click(function() {$(this).nextUntil('h5,h4,h3,h2,h1').slideToggle();} );
 });
