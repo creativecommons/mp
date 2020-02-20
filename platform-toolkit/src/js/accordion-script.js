b2e659f005ae6d825fe568c1fcca5de0bd24fd69
Ana Rocha <apdsrocha@gmail.com>
2020-02-20 19:25:31 -0300

    add up to updating licenses

--- platform-toolkit/src/js/accordion-script.js
+++ platform-toolkit/src/js/accordion-script.js
@@ -4,24 +4,12 @@
   let accordionArray = Array.from(accordion)
   accordionArray.map(item =>
     item.addEventListener("click", function() {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
       this.classList.toggle("active");
-  
-      /* Toggle between hiding and showing the active panel */
       let panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    }));
-
+      panel.style.maxHeight ?
+      panel.style.maxHeight = null :
+      panel.style.maxHeight = panel.scrollHeight + "px"
+    }))
 
   let containerDesktop = doc.querySelectorAll('.pt-chevron');
   let chevronDesktop = doc.querySelectorAll('.chevron-down');
@@ -31,14 +19,12 @@
 
   let addRotation = function( icon ) {
 
-    if(open){
-      icon.className = 'chevron-down open';  
-    } else{
-      icon.className = 'chevron-down';
-    }
+    open ?
+    icon.className = 'chevron-down open' : 
+    icon.className = 'chevron-down';
+    
     open = !open;
   }
-
   containerArray.map((item, index) => item.addEventListener('click', function(){ addRotation( chevronArray[index] )}));
 
 })(document);
