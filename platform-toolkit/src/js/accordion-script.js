b259c518fa013374f6fad2a9c7f2d718d71cd5ff
Ana Rocha <apdsrocha@gmail.com>
2020-03-06 16:50:24 -0300

    add touchstart even to accordion

--- platform-toolkit/src/js/accordion-script.js
+++ platform-toolkit/src/js/accordion-script.js
@@ -2,14 +2,17 @@
   'use strict';
   let accordion = doc.getElementsByClassName("accordion");
   let accordionArray = Array.from(accordion)
-  accordionArray.map(item =>
-    item.addEventListener("click", function() {
-      this.classList.toggle("active");
-      let panel = this.nextElementSibling;
-      panel.style.maxHeight ?
-      panel.style.maxHeight = null :
-      panel.style.maxHeight = panel.scrollHeight + "px"
-    }))
+  function sliding() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    panel.style.maxHeight ?
+    panel.style.maxHeight = null :
+    panel.style.maxHeight = panel.scrollHeight + "px"
+  }
+  accordionArray.map(item => {
+    item.addEventListener("click", sliding)
+    item.addEventListener("touchstart", sliding)
+  })
 
   let containerDesktop = doc.querySelectorAll('.pt-chevron');
   let chevronDesktop = doc.querySelectorAll('.chevron-down');
@@ -20,5 +23,5 @@
     icon.classList.toggle('open')
   }
   containerArray.map((item, index) => item.addEventListener('click', function(){ addRotation( chevronArray[index] )}));
-
+  containerArray.map((item, index) => item.addEventListener('touchstart', function(){ addRotation( chevronArray[index] )}));
 })(document);
